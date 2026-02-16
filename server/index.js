10a2e17ab98d0f6942121b0a70d846c5f1e526fb
Jobeal M Joseph <jobeal2020@gmail.com>
2026-02-16 18:06:43 +0530

    deploy stg 1

--- server/index.js
+++ server/index.js
@@ -6,7 +6,9 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://jobeal-joseph.github.io"
+}));
 app.use(express.json());
 
 // MongoDB Connection
